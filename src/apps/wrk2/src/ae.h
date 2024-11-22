963fee5b73cc5b28af369a5cec4d29335e720b3e
Timothy Mah <timothymah@Timothys-MacBook-Air.local>
2024-11-22 14:13:53 -0500

    added missing files

--- src/apps/wrk2/src/ae.h
+++ src/apps/wrk2/src/ae.h
@@ -30,12 +30,11 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "wrk.h"
 
 #ifndef __AE_H__
 #define __AE_H__
 
-
+#include "wrk.h"
 
 /* Forward declaration to avoid including wrk.h directly */
 struct connection;
