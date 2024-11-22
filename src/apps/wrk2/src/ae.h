7eebf8fb4291f27c770fef271ccd468e12c7344e
Timothy Mah <timothymah@Timothys-MacBook-Air.local>
2024-11-22 13:26:18 -0500

    added missing files

--- src/apps/wrk2/src/ae.h
+++ src/apps/wrk2/src/ae.h
@@ -134,7 +134,9 @@ long long aeCreateTimeEvent(aeEventLoop *eventLoop, long long milliseconds,
         aeEventFinalizerProc *finalizerProc);
 int aeDeleteTimeEvent(aeEventLoop *eventLoop, long long id);
 // int aeProcessEvents(aeEventLoop *eventLoop, int flags);
-int aeProcessEvents(aeEventLoop *eventLoop, thread *thread, int flags);
+// int aeProcessEvents(aeEventLoop *eventLoop, thread *thread, int flags);
+int aeProcessEvents(aeEventLoop *eventLoop, pthread_t *thread, int flags);
+
 int aeWait(int fd, int mask, long long milliseconds);
 // void aeMain(aeEventLoop *eventLoop);
 // void aeMain(aeEventLoop *eventLoop, thread *thread);
