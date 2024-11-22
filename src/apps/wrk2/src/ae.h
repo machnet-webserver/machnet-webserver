64860a1bb4729917a565c75a1bef67fdc25352de
Timothy Mah <timothymah@Timothys-MacBook-Air.local>
2024-11-22 15:07:28 -0500

    added missing files

--- src/apps/wrk2/src/ae.h
+++ src/apps/wrk2/src/ae.h
@@ -1,39 +1,5 @@
-/* A simple event-driven programming library. Originally I wrote this code
- * for the Jim's event-loop (Jim is a Tcl interpreter) but later translated
- * it in form of a library for easy reuse.
- *
- * Copyright (c) 2006-2012, Salvatore Sanfilippo <antirez at gmail dot com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the name of Redis nor the names of its contributors may be used
- *     to endorse or promote products derived from this software without
- *     specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-
 #ifndef __AE_H__
 #define __AE_H__
-
 
 #define AE_OK 0
 #define AE_ERR -1
@@ -53,16 +19,10 @@
 #define AE_NOTUSED(V) ((void) V)
 #include <stdint.h>     // For uint64_t
 
-#include "wrk.h"
-
-
-// Forward declare types from wrk.h instead of including it
-typedef struct thread thread;
-typedef struct connection connection;
+#include "wrk.h"        // Include wrk.h to access thread and connection definitions
 
 /* Function Prototypes */
 typedef struct aeEventLoop aeEventLoop;
-
 
 /* Types and data structures */
 typedef void aeFileProc(struct aeEventLoop *eventLoop, int fd, void *clientData, int mask);
@@ -125,13 +85,11 @@ long long aeCreateTimeEvent(aeEventLoop *eventLoop, long long milliseconds,
         aeTimeProc *proc, void *clientData,
         aeEventFinalizerProc *finalizerProc);
 int aeDeleteTimeEvent(aeEventLoop *eventLoop, long long id);
-// int aeProcessEvents(aeEventLoop *eventLoop, int flags);
 int aeProcessEvents(aeEventLoop *eventLoop, thread *thread, int flags);
 int aeWait(int fd, int mask, long long milliseconds);
-// void aeMain(aeEventLoop *eventLoop);
 void aeMain(aeEventLoop *eventLoop, thread *thread);
 
 char *aeGetApiName(void);
 void aeSetBeforeSleepProc(aeEventLoop *eventLoop, aeBeforeSleepProc *beforesleep);
 
-#endif
+#endif /* __AE_H__ */
