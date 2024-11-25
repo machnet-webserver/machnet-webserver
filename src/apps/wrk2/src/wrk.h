6d260d5235538db4bbf0d92097eae99c81d6b0d7
timothymah <timothymah38@gmail.com>
2024-11-25 12:47:07 -0500

    with pthread

--- src/apps/wrk2/src/wrk.h
+++ src/apps/wrk2/src/wrk.h
@@ -28,9 +28,9 @@
 #define RECVBUF  8192 // was 8192
 #define SAMPLES  100000000 // was 100000000
 
-#define SOCKET_TIMEOUT_MS   2000 // was 2000
+#define SOCKET_TIMEOUT_MS   1000 // was 2000
 #define CALIBRATE_DELAY_MS  10000 //was 10000
-#define TIMEOUT_INTERVAL_MS 2000 // was 2000
+#define TIMEOUT_INTERVAL_MS 250 // was 2000
 
 extern char *local_ip; // Declare global local_ip in net.h
 
