8054bc3c7597b19e40acee4a154ba03d0a52fee8
timothymah <timothymah38@gmail.com>
2024-11-25 12:44:39 -0500

    with pthread

--- src/apps/wrk2/src/wrk.h
+++ src/apps/wrk2/src/wrk.h
@@ -18,12 +18,19 @@
 #include "machnet.h"  // Include Machnet
 
 #define VERSION  "4.0.0"
-#define RECVBUF  4096 // was 8192
-#define SAMPLES  10000 // was 100000000
+// #define RECVBUF  4096 // was 8192
+// #define SAMPLES  10000 // was 100000000
 
-#define SOCKET_TIMEOUT_MS   1000 // was 2000
-#define CALIBRATE_DELAY_MS  2000 //was 10000
-#define TIMEOUT_INTERVAL_MS 250 // was 2000
+// #define SOCKET_TIMEOUT_MS   1000 // was 2000
+// #define CALIBRATE_DELAY_MS  2000 //was 10000
+// #define TIMEOUT_INTERVAL_MS 250 // was 2000
+
+#define RECVBUF  8192 // was 8192
+#define SAMPLES  100000000 // was 100000000
+
+#define SOCKET_TIMEOUT_MS   2000 // was 2000
+#define CALIBRATE_DELAY_MS  10000 //was 10000
+#define TIMEOUT_INTERVAL_MS 2000 // was 2000
 
 extern char *local_ip; // Declare global local_ip in net.h
 
